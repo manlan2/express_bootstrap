161aa30bef7aaa275083085b907e92decd165b17
Ling <panda2ici@gmail.com>
2016-03-31 15:44:55 -0400

    Update order_add_xyj.php

--- order_add_xyj.php
+++ order_add_xyj.php
@@ -15,12 +15,16 @@
     <link href="css/add_order.css" rel="stylesheet" type="text/css">
     <script src="js/api.js"></script>
     <script src="js/order_add.js"></script>
+<script src="js/order_add_xyj.js"></script>
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 <?php require_once ('menu.php'); ?>
 <?php require_once ('login_check.php'); ?>
 <?php session_start();?>
-<div class="container-fluid">
+<div class="container">
+<div class="row">        
+        <div class="col-md-12">
+			<div class="table-responsive">
 	<div class="show_content">
     <table class="tableAdd">
         <tr>
@@ -621,93 +625,50 @@
             </td>
         </tr>
     </table>
-    <input type="button" name="btnTJ" value="创建" id="btnTJ" class="btnAdd"/>
-	</div>
+    <input type="button" name="btn_add_table" value="创建" id="btn_table" class="btnAdd"/>
+	</div></div></div></div>
 </div>
-
-<div class="container">
+<hr>
+<div class="container well">
 	<div class="row">        
         <div class="col-md-12">
-			<h4>Bootstrap Snipp for Datatable</h4>
 			<div class="table-responsive">                
 				<table id="mytable" class="table table-bordred table-striped">
 					<thead>
-						<th><input type="checkbox" id="checkall" /></th>
-						<th>First Name</th>
-						<th>Last Name</th>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Phone</th>
 						<th>Address</th>
-						<th>Email</th>
-						<th>Contact</th>
+						<th>Item1</th>
+						<th>Item2</th>
 						<th>Edit</th>
 						<th>Delete</th>
 					</thead>
 					<tbody>
 						<tr>
-							<td><input type="checkbox" class="checkthis" /></td>
+							<td>1</td>
 							<td>Mohsin</td>
-							<td>Irshad</td>
-							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-							<td>isometric.mohsin@gmail.com</td>
 							<td>+923335586757</td>
+							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+							<td>test1</td>
+							<td>test2</td>
 							<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
 							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
 						</tr>
 							
 						<tr>
-							<td><input type="checkbox" class="checkthis" /></td>
+							<td>2</td>
 							<td>Mohsin</td>
-							<td>Irshad</td>
-							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-							<td>isometric.mohsin@gmail.com</td>
 							<td>+923335586757</td>
+							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
+							<td>test1</td>
+							<td>test2</td>
 							<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
 							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-						</tr>
-							
-						<tr>
-							<td><input type="checkbox" class="checkthis" /></td>
-							<td>Mohsin</td>
-							<td>Irshad</td>
-							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-							<td>isometric.mohsin@gmail.com</td>
-							<td>+923335586757</td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-						</tr>	
-							
-						<tr>
-							<td><input type="checkbox" class="checkthis" /></td>
-							<td>Mohsin</td>
-							<td>Irshad</td>
-							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-							<td>isometric.mohsin@gmail.com</td>
-							<td>+923335586757</td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-						</tr>							
-							
-						<tr>
-							<td><input type="checkbox" class="checkthis" /></td>
-							<td>Mohsin</td>
-							<td>Irshad</td>
-							<td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-							<td>isometric.mohsin@gmail.com</td>
-							<td>+923335586757</td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-							<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-						</tr>					   	
+						</tr>				   	
 					</tbody>				
 				</table>
-				<div class="clearfix"></div>
-				<ul class="pagination pull-right">
-					<li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
-					<li class="active"><a href="#">1</a></li>
-					<li><a href="#">2</a></li>
-					<li><a href="#">3</a></li>
-					<li><a href="#">4</a></li>
-					<li><a href="#">5</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
-				</ul>
+				<a href="#" class="btn btn-block btn-info">Create Excel</a>
             </div>
         </div>
 	</div>
